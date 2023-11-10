22555acf4994581055c3959560b345beb61d7f89
saraV97 <45977173+saraV97@users.noreply.github.com>
2023-11-10 21:00:47 +0530

    Update firebase.js
    removed api keys

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,7 @@ import "firebase/compat/auth";
 import "firebase/compat/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCpVBuBxamH0SyaNDXkKHsdSax2QBBWO10",
-  authDomain: "disney-clone-876ce.firebaseapp.com",
-  projectId: "disney-clone-876ce",
-  storageBucket: "disney-clone-876ce.appspot.com",
-  messagingSenderId: "555579321626",
-  appId: "1:555579321626:web:c96523c5cd28ca20d290a3",
-  measurementId: "G-EZ2LNEVEKL",
+  [API KEYS]
 };
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
